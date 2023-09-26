c035428231eda7ea6e562cccd720a7eb40004056
Miguel Bernal <65865138+mibernal@users.noreply.github.com>
2023-09-26 15:10:20 -0500

    finalizando app
    finalizando app

--- forms.py
+++ forms.py
@@ -4,4 +4,12 @@ from .models import Idea
 class IdeaForm(forms.ModelForm):
     class Meta:
         model = Idea
-        fields = ['title', 'description', 'image']
+        fields = ['title', 'description', 'image', 'software_idea']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Personaliza los widgets si es necesario
+        self.fields['title'].widget = forms.TextInput(attrs={'class': 'form-control'})
+        self.fields['description'].widget = forms.Textarea(attrs={'class': 'form-control'})
+        self.fields['image'].widget = forms.ClearableFileInput(attrs={'class': 'form-control-file'})
+        self.fields['software_idea'].widget = forms.Textarea(attrs={'class': 'form-control'})
